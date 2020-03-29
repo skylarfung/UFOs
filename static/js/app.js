8a63ceb59b041cd154ec14b2ce9894f6da02c2c6
Skylar Fung <skylarfung@outlook.com>
2020-03-28 22:59:29 -0700

    working button that filters data by date

--- static/js/app.js
+++ static/js/app.js
@@ -5,23 +5,27 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 // function takes the data and build a table
-function tableBuild(data) {
+function buildTable(data) {
     tbody.html("");
-    data.foreach((dataRow) => {
+    data.forEach((dataRow) => {
         let row = tbody.append("tr");
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
             cell.text(val);
         });
     });
-}
+};
 
+//function to handle click
 function handleClick() {
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
     if (date) {
-        filteredData = filteredData.filter(row => row.datetime ===data);
+        filteredData = filteredData.filter(row => row.datetime ===date);
     };
-    d3.select("#filter-btn").on("click", handleClick);
-    buildTable(tableData);
-}
+    buildTable(filteredData);
+    console.log("Button was pressed");
+};
+
+// what happens when button is clicked 
+d3.select("#filter-btn").on("click", handleClick);
