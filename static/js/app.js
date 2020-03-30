a2fcea7c44792ee466b2015a1eaf557be0e65cef
Skylar Fung <skylarfung@outlook.com>
2020-03-29 19:17:48 -0700

    filters are saved to a filter object then applied to table

--- static/js/app.js
+++ static/js/app.js
@@ -5,7 +5,7 @@ const tableData = data;
 var tbody = d3.select("tbody");
 
 //varable to hold filters
-var filters ={};
+var filters = {};
 
 // function takes the data and build a table
 function buildTable(data) {
@@ -19,9 +19,62 @@ function buildTable(data) {
     });
 };
 
-//new function to handle click
+//new function to handle click and add filters
 function updateFilters() {
+    //saves value of filter
+    let date = d3.select("#datetime").property("value");
+    let city = d3.select("#city").property("value");
+    let state = d3.select("#state").property("value");
+    let country = d3.select("#country").property("value");
+    let shape = d3.select("#shape").property("value");
 
+    //adds filters to filters object 
+    if (date) {
+        filters.datetime = date
+    } else {
+        delete filters.datetime
+    };
+
+    if (city) {
+        filters.city = city
+    } else {
+        delete filters.city
+    };
+
+    if (state) {
+        filters.state = state
+    } else {
+        delete filters.state
+    };
+
+    if (country) {
+        filters.country = country
+    } else {
+        delete filters.country
+    };
+
+    if (shape) {
+        filters.shape = shape
+    } else {
+        delete filters.shape
+    };
+
+    console.log(filters);
+    filterTable();
+};
+
+function filterTable() {
+    let filteredData = tableData;
+
+    for (var key in filters) {
+        filteredData = filteredData.filter(row => row.datetime === filters[key]);
+        filteredData = filteredData.filter(row => row.city === filters[key]);
+        filteredData = filteredData.filter(row => row.state === filters[key]);
+        filteredData = filteredData.filter(row => row.country === filters[key]);
+        filteredData = filteredData.filter(row => row.shape === filters[key]);
+    };
+
+    buildTable(filteredData);
 };
 
 //function to handle click
@@ -36,4 +89,4 @@ function handleClick() {
 };
 
 // what happens when button is clicked 
-d3.select("#filter-btn").on("click", handleClick);
+d3.select("#filter-btn").on("click", updateFilters);
